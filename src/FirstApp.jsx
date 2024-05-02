8fa6754680e3c715515535db909aeb16a498b39d
israel <israel.bejarano.a@gmail.com>
2024-05-02 11:32:46 +0200

    feat: impresion de variables

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,13 @@
-export function FirstApp() {
+
+const getResult = (a, b) => {
+    return a+b;
+}
+
+
+export const FirstApp = () => {
     return (
         <>
+            <h1>{getResult(1,2)}</h1>
             <h2>Soy un ejercicio!</h2>
             <p>esto es para ver el uso de Fragment</p>
         </>
