df0d5953cdda445804cbb5dbe105d5c4a6e85d4f
israel <israel.bejarano.a@gmail.com>
2024-05-02 11:41:08 +0200

    feat: uso de estilos css globales

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HelloWorldApp} from './HelloWorldApp'
 import {FirstApp} from './FirstApp'
+import './styles.css';
+
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(
