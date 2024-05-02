023994e84d6db6291b42dbe282095ce3cdd89bfb
israel <israel.bejarano.a@gmail.com>
2024-05-02 19:30:13 +0200

    feat: default props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -4,7 +4,7 @@ const getResult = (a, b) => {
 }
 
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({title, subTitle, name}) => {
     return (
         <>
             <h1>{title}</h1>
@@ -12,6 +12,7 @@ export const FirstApp = ({title, subTitle}) => {
             <h1>{getResult(1,2)}</h1>
             <h2>Soy un ejercicio!</h2>
             <p>esto es para ver el uso de Fragment</p>
+            <p>{name}</p>
         </>
 );
 }
@@ -19,4 +20,10 @@ export const FirstApp = ({title, subTitle}) => {
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
     subTitle: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+    title: 'título por defecto',
+    subTitle: 0,
+    name: 'Israel Bejarano Álvrez'
 }
