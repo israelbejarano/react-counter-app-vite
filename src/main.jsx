da2e61129c615cb91b46da2c5455e3379ddb5ce5
israel <israel.bejarano.a@gmail.com>
2024-05-02 19:38:33 +0200

    feat: counterApp component

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HelloWorldApp} from './HelloWorldApp'
 import {FirstApp} from './FirstApp'
+import { CounterApp } from './CouterApp';
 import './styles.css';
 
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
-        <HelloWorldApp/>
-        <FirstApp title="Hola, soy John" subTitle={123}/>
+        {/* <HelloWorldApp/>
+        <FirstApp title="Hola, soy John" subTitle={123}/> */}
+        <CounterApp value={1989}/>
     </React.StrictMode>
 );
