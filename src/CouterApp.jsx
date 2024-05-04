f6b2be30ac496f39c1084c938240ceaa22474fa8
israel <israel.bejarano.a@gmail.com>
2024-05-04 12:23:43 +0200

    feat: uso del hook useState

--- src/CouterApp.jsx
+++ src/CouterApp.jsx
@@ -1,11 +1,18 @@
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
+    const [counter, setCounter] = useState(value);
+    const handleAdd = () => {
+        // setCounter(counter +1);
+        setCounter((c) => c+1);
+        console.log(counter);
+    }
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
-            <button></button>
+            <h2> { counter } </h2>
+            <button onClick={handleAdd}>+1</button>
         </>
     );
 }
